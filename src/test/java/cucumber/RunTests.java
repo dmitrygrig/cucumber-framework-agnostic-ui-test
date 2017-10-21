352ff7f5aca61079cb410ecd3dcdfdd8857430a6
Dmytro Grygorenko <dmitrygrig@gmail.com>
2017-10-22 01:30:14 +0200

    webdriver is configured in gradle

--- src/test/java/cucumber/RunTests.java
+++ src/test/java/cucumber/RunTests.java
@@ -1,11 +1,9 @@
 package cucumber;
 
-import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"html:target/Destination"})
 public class RunTests {
 
 }
